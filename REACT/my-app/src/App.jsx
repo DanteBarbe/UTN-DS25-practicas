eab4f813a88a07c866e5eb8d05dbae04adbee1cf
Dante Manuel Barbé Lorenzo <dante_0312@hotmail.com>
2025-06-03 17:13:22 -0300

    Agrego ScienceFictionApp

--- REACT/my-app/src/App.jsx
+++ REACT/my-app/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeApp from './Apps/HomeApp'
 import TerrorApp from './Apps/TerrorApp'
 import WWIIApp from './Apps/WWIIApp';
+import LiteraryApp from './Apps/LiteraryApp';
+import ScienceFictionApp from './Apps/ScienceFictionApp';
 
 
   function App() {
@@ -13,6 +15,7 @@ import WWIIApp from './Apps/WWIIApp';
         <Route path="/terror" element={<TerrorApp />} />
         <Route path="/wwII" element={<WWIIApp />} />
         <Route path="/literary" element={<LiteraryApp />} />
+        <Route path="/science-fiction" element={<ScienceFictionApp />} />
       </Routes>
     </Router>
     </>
@@ -21,7 +24,5 @@ import WWIIApp from './Apps/WWIIApp';
 
 export default App;
 /*
-<Route path="/science-fiction" element={<ScienceFiction />} />
-<Route path="/wwII" element={<WWII />} />
 <Route path="/contact" element={<Contact />} />
 <Route path="/register" element={<Register />} />*/
